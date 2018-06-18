5b08674593dce8cf2b62002f396cae935b1acbc5
Robin Melanson <remcogold@gmail.com>
2018-06-18 00:12:00 -0400

    Update sshConnectBootstrap.sh

--- sshConnectBootstrap.sh
+++ sshConnectBootstrap.sh
@@ -16,9 +16,9 @@ pkg=Web
 gitRepo="linux-scripts-utils-gitHub-sshConnect"
 installDir="/tmp/scripts/utils/sshConnect"
 if [ -f ~/.ssh/gitHub.key ]; then
-   clone="git clone -b $branch git@github.com:RMelanson/"
+   clone="git clone git@github.com:RMelanson/"
 else
-   clone="git clone -b $branch https://github.com/RMelanson/"
+   clone="git clone https://github.com/RMelanson/"
 fi
 
 # Clone $pkg
