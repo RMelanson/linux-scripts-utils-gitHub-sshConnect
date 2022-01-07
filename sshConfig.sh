e037b320cf1c7910cb5efba12753414b050070a7
Robin Melanson <robin.e.melanson@gmail.com>
2022-01-07 13:12:19 -0500

    Update sshConfig.sh

--- sshConfig.sh
+++ sshConfig.sh
@@ -5,6 +5,15 @@ echo " IdentityFile ~/.ssh/gitHub"     >> ~/.ssh/config
 echo " User git"                       >> ~/.ssh/config
 echo " passwordAuthentication no"      >> ~/.ssh/config
 chmod 600 ~/.ssh/config
+
+if grep -Fxq ~/.bashrc "ssh-agent"
+then
+    echo ssh-agent found
+else
+    echo ssh-agent not found, Adding ssh-agent
+    echo EXECUTING "echo 'eval \$("ssh-agent")' >> ~/.bashrc"
+    echo 'eval $("ssh-agent")' >> ~/.bashrc
+fi
+
 echo "<Copy the following ssh public key to github settings for ssh keys>"
 cat ~/.ssh/gitHub.pub
-echo
