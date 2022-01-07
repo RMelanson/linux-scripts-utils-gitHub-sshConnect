b7476bf641ddbe42c236c926cee54bdcb6d251c5
Robin Melanson <robin.e.melanson@gmail.com>
2022-01-07 01:48:17 -0500

    Update sshConfig.sh

--- sshConfig.sh
+++ sshConfig.sh
@@ -1,7 +1,7 @@
-ssh-keygen -t rsa -N "" -f ~/.ssh/gitHub.key
+ssh-keygen -t rsa -N "" -f ~/.ssh/gitHub
 echo "host github.com"                 >> ~/.ssh/config
 echo " HostName github.com"            >> ~/.ssh/config
-echo " IdentityFile ~/.ssh/gitHub.key" >> ~/.ssh/config
+echo " IdentityFile ~/.ssh/gitHub"     >> ~/.ssh/config
 echo " User git"                       >> ~/.ssh/config
 echo " passwordAuthentication no"      >> ~/.ssh/config
 echo "<Copy the following ssh public key to github settings for ssh keys>"
