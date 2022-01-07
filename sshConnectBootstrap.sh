556fc353ea6273660e2b39c8c034a1a9ea12e736
Robin Melanson <robin.e.melanson@gmail.com>
2022-01-07 03:53:21 -0500

    Update sshConnectBootstrap.sh

--- sshConnectBootstrap.sh
+++ sshConnectBootstrap.sh
@@ -12,58 +12,53 @@ if [ "$EUID" -ne 0 ]
   if [ "$(getent group wheel | grep $(whoami))" = "" ]
     then
       echo $(whoami) HAS NO SUDO ACCESS 2>&1 | tee -a wordpressBootstrap.log
-      echo "ERROR: CANNOT PROCEED, MUST RUN AS ROOT OR UNDER SUDO" 2>&1 | tee -a sshConnectBootstrap.log
-    else
+  else
       echo $(whoami) HAS SUDO 2>&1 | tee -a sshConnectBootstrap.log
-      sudo $0 $@
+      #INITIAL BASIC TOOLS INSTALL
+      echo updating System
+      sudo yum update -y
+
+      #INSTALL GIT
+      echo installing git if not installed
+      sudo yum install git -y
   fi
 else
-   if [ "$EUID" -ne 0 ]
-   then
-      sudo -n true 2/dev/null 2>&1
-      passwordRequired=$?
-
-      if [ "$passwordRequired" == "1" ]; then
-          echo "Please run as root or under user with sudo access sudo"
-      else
-          sudo chmod +x $bootstrap
-          sudo $bootstrap
-      fi
-      return 1
-   fi
-
+   echo UPDATING SYSTEM AS ROOT
    #INITIAL BASIC TOOLS INSTALL
+   echo updating System
    yum update -y
 
    #INSTALL GIT
+   echo installing git if not installed
    yum install git -y
+fi
+chmod +x $bootstrap
+$bootstrap
 
-   # SETUP ENVIRONMENT AND PARAMETERS
-   sshConnectDir=$PWD
-   pkg=SSH_CONNECT
-   gitRepo="linux-scripts-utils-gitHub-sshConnect.git"
-   installDir="/tmp/scripts/utils/$pkg"
-   remoteHostName=gitHub
+# SETUP ENVIRONMENT AND PARAMETERS
+sshConnectDir=$PWD
+pkg=SSH_CONNECT
+gitRepo="linux-scripts-utils-gitHub-sshConnect.git"
+installDir="/tmp/scripts/utils/$pkg"
+remoteHostName=gitHub
 
-   if [ -f ~/.ssh/$gitHub ]; then
-      clone="git clone git@github.com:RMelanson/"
-   else
-      clone="git clone https://github.com/RMelanson/"
-   fi
-
-   # Clone $pkg
-   echo Executing $clone$gitRepo $installDir
-   $clone$gitRepo $installDir
-   # MAKE ALL SHELL SCRIPTS EXECUTABLE TO ROOT ONLY
-   find . -name "*.sh" -exec chmod 700 {} \;
-
-   # Setup Project
-   echo "BOOTSTRAP EXECUTING: ./setup.sh $* 2>&1| tee setup.log"
-   ./setup.sh $* 2>&1| tee setup.log
-
-   cd $bootstrapDir
+if [ -f ~/.ssh/$gitHub ]; then
+   clone="git clone git@github.com:RMelanson/"
+else
+   clone="git clone https://github.com/RMelanson/"
 fi
 
+# Clone $pkg
+echo Executing $clone$gitRepo $installDir
+$clone$gitRepo $installDir
+# MAKE ALL SHELL SCRIPTS EXECUTABLE TO ROOT ONLY
+find . -name "*.sh" -exec chmod 700 {} \;
 
-   # Setup $pkg
-   cd $bootstrapDir
+# Setup Project
+echo "BOOTSTRAP EXECUTING: ./setup.sh $* 2>&1| tee setup.log"
+./setup.sh $* 2>&1| tee setup.log
+
+cd $bootstrapDir
+
+# Setup $pkg
+cd $bootstrapDir
