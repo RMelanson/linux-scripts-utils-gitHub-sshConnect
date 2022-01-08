1ebe9847e0700e2a7d5b8afc9553368ed829010e
Robin Melanson <robin.e.melanson@gmail.com>
2022-01-08 16:05:39 -0500

    Update sshConnectBootstrap.sh

--- sshConnectBootstrap.sh
+++ sshConnectBootstrap.sh
@@ -4,8 +4,16 @@
 sshConnectDir=$PWD
 pkg=SSH_CONNECT
 gitRepo="linux-scripts-utils-gitHub-sshConnect.git"
-installDir="/tmp/scripts/utils/$pkg"
+installDir="/tmp/scripts/utils/$pkg"/
 remoteHostName=gitHub
+
+echoLog(){
+ echo $* 2>&1 | tee setup.log
+}
+ 
+is_root_user(){
+  [ $(id -u) -eq 0 ]
+}
 
 # is_root_user: Determine if current user is root (id = 0)
 is_root_user(){
@@ -19,31 +27,34 @@ is_sudo_user(){
 
 # execute_as_root: execute as root if root user or has sudo access
 execute_as_root(){
+  echoLog "EXECUTING: "$@
   if is_root_user
   then {
-    echo EXECUTING AS ROOT USER command $*
+    echoLog EXECUTING AS ROOT USER command $*
     $*
   }
   else {
     if is_sudo_user
     then {
-      echo EXECUTING AS SUDO USER $*
+      echoLog EXECUTING sudo $*
       sudo $*
     }
     else {
-      echo NO ROOT OR SUDO ACCESS FOR USER $(whoami)
-      echo COMMAND $*
-      echo NOT EXECUTED UNDER ROOT
+      echoLog NO ROOT OR SUDO ACCESS FOR USER $(whoami)
+      echoLog COMMAND $*
+      echoLog NOT EXECUTED UNDER ROOT
     }
     fi
   }
   fi
 }
 update_system(){
+   echoLog("Updating System: execute_as_root yum update -y");
    execute_as_root yum update -y
 }
 
 install_git(){
+   echoLog("Installing git");
    execute_as_root yum install git -y
 }
 
@@ -59,14 +70,14 @@ configure_remote_ssh_access(){
   # Add ssh-agent to bashrc if not already installed for load on startup
   if grep -Fxq ~/.bashrc "ssh-agent"
   then
-      echo ssh-agent found
+      echoLog ssh-agent found
   else
-      echo ssh-agent not found, Adding ssh-agent to bashrc starup script
-      echo EXECUTING "echo 'eval \$("ssh-agent")' >> ~/.bashrc"
+      echoLog ssh-agent not found, Adding ssh-agent to bashrc starup script
+      echoLog EXECUTING "echo 'eval \$("ssh-agent")' >> ~/.bashrc"
       echo 'eval $("ssh-agent")' >> ~/.bashrc
   fi
 
-  echo "<Copy the following ssh public (~/.ssh/gitHub.pub) key to the remote authorized keys keys>"
+  echoLog "<Copy the following ssh public (~/.ssh/gitHub.pub) key to the remote authorized keys keys>"
   cat ~/.ssh/gitHub.pub
 }
 
